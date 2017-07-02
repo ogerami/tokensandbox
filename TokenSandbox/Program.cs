6c4dbfc50509ff3765edf8603d68c43cd3cded42
GERAMI-TABLET\omid <odawggy007@gmail.com>
2017-07-02 01:00:09 -0400

    Initial Commit

---
+++ TokenSandbox/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TokenSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = "1[1[cma]],2[1[cma],2[100,200],3[xxx,xxx]],3,4";
+
+            var split = token.Split(',');
+
+            var tokenlist = new List<string>();
+            var stringitem = "";
+
+            foreach (var s in split)
+            {
+                if (s.Length > 1)
+                {
+                    stringitem += s;
+                    if (!s.EndsWith("]]"))
+                    {
+                        stringitem += ",";
+                        continue;
+                    };
+                    tokenlist.Add(stringitem);
+                    stringitem = "";
+                }
+                else
+                {
+                    tokenlist.Add(s);
+                }
+            }
+            
+            var transformedToken = string.Join("|", tokenlist.Select(s => s.Replace("],", "]^")));
+
+            Console.WriteLine(transformedToken);
+
+            var tokenEqual = "1[1[cma]]|2[1[cma]^2[100,200]^3[xxx,xxx]]|3|4";
+
+            Console.WriteLine(transformedToken == tokenEqual);
+
+            
+            Console.Read();
+        }
+    }
+}
